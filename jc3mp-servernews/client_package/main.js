7d6f6b1f4747d3aeec6f883e2e3a7a52ff190885
Lukas Berwanger <lukas.berwanger@googlemail.com>
2016-12-27 02:28:42 +0100

    API changes

--- jc3mp-servernews/client_package/main.js
+++ jc3mp-servernews/client_package/main.js
@@ -10,6 +10,6 @@
 const ui = new WebUIWindow('jc3mp-servernews', 'package://jc3mp-servernews/ui/index.html', new Vector2(jcmp.viewportSize.x, jcmp.viewportSize.y));
 ui.autoResize = true;
 
-jcmp.ui.AddEventHandler('toggle_cursor', function (toggle) {
-    localPlayer.controlsEnabled = toggle;
+jcmp.ui.AddEvent('toggle_cursor', (toggle) => {
+    jcmp.localPlayer.controlsEnabled = toggle;
 });
